93a728c043cfa294181d070415666d2e20f22e9d
elmiraaemi <108354246+elmiraaemi@users.noreply.github.com>
2023-09-08 01:15:47 +0330

    HangMan.py

--- HangMan.py
+++ HangMan.py
@@ -17,16 +17,25 @@ while joon > 0:
             while user_character in w:
                 answer[w.index(user_character)] = user_character
                 w[w.index(user_character)] = ''
-                print("yor wrngs : ", wrongs)
+                print("your wrongs : ", wrongs)
             if '-' not in answer: break
-        else:
+        elif user_character not in w and user_character not in wrongs:
              joon -= 1
              wrongs.append(user_character)
-             print("yor wrngs : ", wrongs)
+             print("your wrongs : ", wrongs)
+        elif user_character in wrongs:
+            print("you have made this mistake before @_@")
+            print("your wrongs : ", wrongs)
+            print("\nyour chances : " , joon )
+        elif user_character in w and user_character not in wrongs:
+            print("you already typed this letter")
+            print("your wrongs : ", wrongs)
+            print("\nyour chances : " , joon )
     else :
         print("just one letter -_-")
 if joon==0: 
     print("You lost that -~-")
 else: 
     for j in range(len(answer)): print(answer[j], end=' ')
+    print('\nyes , you won *.*')
     print('\nyes , you won *.*')
